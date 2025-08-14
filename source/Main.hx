8895b0bfe62b6b023a475e49ef2fb5739cafba75
NAEL2XD <108278470+NAEL2XD@users.noreply.github.com>
2025-08-14 16:53:03 +0200

    fully add all ptmu service and minifed main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,36 +1,21 @@
-import hx3ds.FS.MediaType;
-import hx3ds.OS;
-import hx3ds.AM;
-import hx3ds.CFGU;
-import hx3ds.PTMU;
 import hx3ds.HID;
+import hx3ds.APT;
 import hx3ds.Console;
 import hx3ds.GFX;
-import hx3ds.APT;
 
 function main() {
 	untyped __include__("3ds.h");
-	
+
 	GFX.initDefault();
-	PTMU.init();
-	CFGU.init();
-	AM.init();
 	Console.init(GFX_TOP);
+	trace("Hello, World!");
 
-	OS.setSpeedupEnable(APT.isNew3DS());
-
-	trace('${ConsoleColor.textRed}Hello, ${ConsoleColor.textGreen}${ConsoleColor.borderWhite}World!${ConsoleColor.borderBlack}${ConsoleColor.textWhite}');
-	trace('Press [START] to exit.');
-	
 	while (APT.mainLoop()) {
 		HID.scanInput();
 		if (HID.keyPressed(Key.START)) {
 			break;
 		}
 	}
-	
-	AM.exit();
-	CFGU.exit();
-	PTMU.exit();
+
 	GFX.exit();
 }
